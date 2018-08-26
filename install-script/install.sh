37fd20c1a106619c77e480c8331636d87a5957ee
Arunscape <arunscape@gmail.com>
2018-08-26 15:23:59 -0600

    install script layout

---
+++ install-script/install.sh
@@ -0,0 +1,39 @@
+# reminder of variables
+#DRIVE=''
+#HOSTNAME=''
+#ROOT_PASSWD=''
+#USERNAME=''
+USER_PASSWD=''
+#TIMEZONE=''
+#KEYMAP=''
+
+
+# load variables
+. variables
+
+
+setup(){
+
+}
+
+format(){
+
+}
+
+mount(){
+
+}
+
+pacstrap(){
+
+}
+
+post_pacstrap(){
+
+}
+
+setup
+format
+mount
+pacstrap
+post_pacstrap

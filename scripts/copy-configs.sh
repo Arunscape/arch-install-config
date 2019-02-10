08ba06f3e120b91ed53e42f396ea7d16610a3e97
Arunscape <8227297+Arunscape@users.noreply.github.com>
2019-02-09 23:28:13 -0700

    some updates

--- scripts/copy-configs.sh
+++ scripts/copy-configs.sh
@@ -1,7 +1,10 @@
 # This is a script to backup my configs
 
 # i3 config
-cp ~/.config/i3/config ~/Downloads/arch-install-config/configs/home/.config/i3/config
+#cp ~/.config/i3/config ~/Downloads/arch-install-config/configs/home/.config/i3/config
+
+# sway config
+cp ~/.config/sway/config ~/Downloads/arch-install-config/configs/home/.config/sway/config
 
 # xinitrc
 cp ~/.xinitrc ~/Downloads/arch-install-config/configs/home/.xinitrc
@@ -14,3 +17,6 @@ cp ~/.vimrc ~/Downloads/arch-install-config/configs/home/.vimrc
 
 #libinput gestures
 cp ~/.config/libinput-gestures.conf ~/Downloads/arch-install-config/configs/home/.config/libinput-gestures.conf 
+
+# kitty
+cp ~/.config/kitty/kitty.conf ~/Downloads/arch-install-config/configs/home/.config/kitty/kitty.conf

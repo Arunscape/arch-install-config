1d200e1c02a9b7078114f057e53c77c6fb625473
Arunscape <8227297+Arunscape@users.noreply.github.com>
2019-08-26 10:30:30 -0600

    linting fix + formatting

--- install-scripts/01-install.sh
+++ install-scripts/01-install.sh
@@ -36,218 +36,220 @@ WIFI=''
 # KEYMAP=''
 
 setup(){
-
-	if [ -z "$DRIVE" ]
+    
+    if [ -z "$DRIVE" ]
+    then
+        echo 'Enter the drive to install Arch to:'
+        read DRIVE
+    fi
+    echo Installing Arch to: $DRIVE
+    
+    if [ -z "$HOST_NAME" ]
+    then
+        echo 'Enter your desired hostname:'
+        read HOST_NAME
+    fi
+    echo Hostname: $HOST_NAME
+    
+    #if [ -z "$ROOT_PASSWD" ]
+    #then
+    #   echo 'Enter the root password:'
+    #   read ROOT_PASSWORD
+    #fi
+    
+    if [ -z "$USERNAME" ]
+    then
+        echo 'Enter your username:'
+        read USERNAME
+    fi
+    echo Username: $USERNAME
+    
+    if [ -z "$USER_PASSWD" ]
+    then
+        echo 'Enter your password:'
+        read USER_PASSWD
+    fi
+    while true
+    do
+        echo "Confirm your password: "
+        read passwdconfirmation
+        
+        if [ "$USER_PASSWD" == "$passwdconfirmation" ]
         then
-       	   echo 'Enter the drive to install Arch to:'
-           read DRIVE
+            echo Cool
+            break
+            
+        else
+            echo That wasn\'t correct, try again, or press Ctrl+C to exit the script, change your password, and re-run this script
         fi
-	echo Installing Arch to: $DRIVE
-
-	if [ -z "$HOST_NAME" ]
-        then
-       	   echo 'Enter your desired hostname:'
-           read HOST_NAME
-        fi
-	echo Hostname: $HOST_NAME
-
-	#if [ -z "$ROOT_PASSWD" ]
-        #then
-       	#   echo 'Enter the root password:'
-        #   read ROOT_PASSWORD
-        #fi
-
-	if [ -z "$USERNAME" ]
-        then
-       	   echo 'Enter your username:'
-           read USERNAME
-        fi
-	echo Username: $USERNAME
-
-	if [ -z "$USER_PASSWD" ]
-        then
-       	   echo 'Enter your password:'
-           read USER_PASSWD
-        fi
-	while true
-	do
-		echo "Confirm your password: "
-		read passwdconfirmation
-
-		if [ "$USER_PASSWD" == "$passwdconfirmation" ]
-        	then
-       	   		echo Cool
-			break
-
-		else
-			echo That wasn\'t correct, try again, or press Ctrl+C to exit the script, change your password, and re-run this script
-		fi
-	done
-
-	if [ -z "$TIMEZONE" ]
-        then
-       	   echo 'Enter your timezone'
-           read TIMEZONE
-        fi
-	echo Timezone: $TIMEZONE
-
-	#if [ -z "$KEYMAP" ]
-        #then
-       	#   echo 'Enter your keymap'
-        #   read KEYMAP
-        #fi
-
-	if [ -z "$CPU" ]
-		while true
-		do
-			echo "type amd or intel (all lowercase, exactly as you see on the left) "
-			read passwdconfirmation
-
-			if [ "$CPU" == "amd" ] || [ "$CPU" == "intel"]
-        		then
-       	   			echo Installing for $CPU...
-				break
-
-			else
-				echo Type either amd or intel exactly
-			fi
-		done
-
-	if [ -z "$WIFI" ]
-	then
-		echo 'Do you need wifi? If not, just hit enter. If you do, type anything then hit enter'
-		read WIFI
-	fi
+    done
+    
+    if [ -z "$TIMEZONE" ]
+    then
+        echo 'Enter your timezone'
+        read TIMEZONE
+    fi
+    echo Timezone: $TIMEZONE
+    
+    #if [ -z "$KEYMAP" ]
+    #then
+    #   echo 'Enter your keymap'
+    #   read KEYMAP
+    #fi
+    
+    if [ -z "$CPU" ]
+    then
+        while true
+        do
+            echo "type amd or intel (all lowercase, exactly as you see on the left) "
+            read passwdconfirmation
+            
+            if [ "$CPU" == "amd" ] || [ "$CPU" == "intel"]
+            then
+                echo Installing for $CPU...
+                break
+                
+            else
+                echo Type either amd or intel exactly
+            fi
+        done
+        
+    fi
+    if [ -z "$WIFI" ]
+    then
+        echo 'Do you need wifi? If not, just hit enter. If you do, type anything then hit enter'
+        read WIFI
+    fi
 }
 
 format(){
-	read -p "$(tput bold)$(tput setaf 1)WARNING this will wipe $DRIVE Press ENTER to continue, or Ctrl+C to exit$(tput sgr 0)"
-
-	# for some reason, fdisk still detects filesystem signatures
-	# wipefs -af $DRIVE
-
-	# for ssd
-	blkdiscard $DRIVE
-
-	# use fdisk to partition drives
-	(
-	echo g           # create GPT partition table
-	echo n           # create /boot partision
-	echo             # accept default partition number 1
-	echo             # accept default first sector
-	echo +$BOOTSIZE  # EFI partition is 100M
-	echo t           # change partition type to EFI
-	echo 1           #
-	echo n           # root partition, 32G
-	echo             #
-	echo             #
-	echo +$ROOTSIZE   #
-	echo n           # SWAP partition, 24G
-	echo             #
-	echo             #
-	echo +$SWAPSIZE  #
-	echo n           # /home parition, fill rest of disk
-	echo             #
-	echo             #
-	echo             #
-	echo p           # show what's going to be done
-	echo w           # write changes
-	echo q           # quit fdisk
-	) | fdisk $DRIVE
-
-
-	local bootpart="$DRIVE"1
-	local rootpart="$DRIVE"2
-	local swappart="$DRIVE"3
-	local homepart="$DRIVE"4
-
-	# format partitions
-	mkfs.fat $bootpart  # /boot
-	mkfs.ext4 $rootpart # /
-	mkswap $swappart    # SWAP
-	swapon $swappart    #
-	mkfs.ext4 $homepart # /home
-
-	#mount the partitions
-	mount $rootpart /mnt
-	mkdir /mnt/boot
-	mkdir /mnt/home
-	mount $bootpart /mnt/boot
-	mount $homepart /mnt/home
-
-	#echo "Do you want to edit the mirrorlist? y/n"
-	#read editmirrorlist
-	#if [[ "$choice" == [Yy]* ]]
-	#then
-	#	vim /etc/pacman.d/mirrorlist
-	#fi
-
-	# this mirror works fast enough for me
-	# echo 'Server = https://mirrors.kernel.org/archlinux/$repo/os/$arch' > /etc/pacman.d/mirrorlist
-	# turns out that this is the default mirror anyways
+    read -p "$(tput bold)$(tput setaf 1)WARNING this will wipe $DRIVE Press ENTER to continue, or Ctrl+C to exit$(tput sgr 0)"
+    
+    # for some reason, fdisk still detects filesystem signatures
+    # wipefs -af $DRIVE
+    
+    # for ssd
+    blkdiscard $DRIVE
+    
+    # use fdisk to partition drives
+    (
+        echo g           # create GPT partition table
+        echo n           # create /boot partision
+        echo             # accept default partition number 1
+        echo             # accept default first sector
+        echo +$BOOTSIZE  # EFI partition is 100M
+        echo t           # change partition type to EFI
+        echo 1           #
+        echo n           # root partition, 32G
+        echo             #
+        echo             #
+        echo +$ROOTSIZE   #
+        echo n           # SWAP partition, 24G
+        echo             #
+        echo             #
+        echo +$SWAPSIZE  #
+        echo n           # /home parition, fill rest of disk
+        echo             #
+        echo             #
+        echo             #
+        echo p           # show what's going to be done
+        echo w           # write changes
+        echo q           # quit fdisk
+    ) | fdisk $DRIVE
+    
+    
+    local bootpart="$DRIVE"1
+    local rootpart="$DRIVE"2
+    local swappart="$DRIVE"3
+    local homepart="$DRIVE"4
+    
+    # format partitions
+    mkfs.fat $bootpart  # /boot
+    mkfs.ext4 $rootpart # /
+    mkswap $swappart    # SWAP
+    swapon $swappart    #
+    mkfs.ext4 $homepart # /home
+    
+    #mount the partitions
+    mount $rootpart /mnt
+    mkdir /mnt/boot
+    mkdir /mnt/home
+    mount $bootpart /mnt/boot
+    mount $homepart /mnt/home
+    
+    #echo "Do you want to edit the mirrorlist? y/n"
+    #read editmirrorlist
+    #if [[ "$choice" == [Yy]* ]]
+    #then
+    #	vim /etc/pacman.d/mirrorlist
+    #fi
+    
+    # this mirror works fast enough for me
+    # echo 'Server = https://mirrors.kernel.org/archlinux/$repo/os/$arch' > /etc/pacman.d/mirrorlist
+    # turns out that this is the default mirror anyways
 }
 
 
 run_pacstrap(){
-	pacman -Syy
-	pacstrap /mnt base base-devel
+    pacman -Syy
+    pacstrap /mnt base base-devel
 }
 
 chroot_step(){
-
-	# so that linux auto mounts /root /boot /home
-	genfstab -U /mnt >> /mnt/etc/fstab
-	curl -Lo /mnt/install.sh https://raw.githubusercontent.com/Arunscape/arch-install-config/master/install-scripts/02-chroot.sh
-	chmod +x /mnt/install.sh
-	arch-chroot /mnt bash install.sh $USERNAME $USER_PASSWD $HOST_NAME $TIMEZONE $DRIVE $CPU $WIFI
+    
+    # so that linux auto mounts /root /boot /home
+    genfstab -U /mnt >> /mnt/etc/fstab
+    curl -Lo /mnt/install.sh https://raw.githubusercontent.com/Arunscape/arch-install-config/master/install-scripts/02-chroot.sh
+    chmod +x /mnt/install.sh
+    arch-chroot /mnt bash install.sh $USERNAME $USER_PASSWD $HOST_NAME $TIMEZONE $DRIVE $CPU $WIFI
 }
 
 # copy_configs(){
 
-	# echo Copying configs...
+# echo Copying configs...
 
-	# local HOMEDIR='/mnt/home/'$USERNAME
-	
-	# make directories as necessary
-	# mkdir -p $HOMEDIR/.config/kitty
-	# mkdir -p $HOMEDIR/.config/sway
-	
-	# sway config
-	# curl -Lo $HOMEDIR/.config/sway/config https://raw.githubusercontent.com/Arunscape/arch-install-config/master/configs/home/.config/sway/config
+# local HOMEDIR='/mnt/home/'$USERNAME
 
-	# kitty
-	# curl -Lo $HOMEDIR/.config/kitty/kitty.conf https://raw.githubusercontent.com/Arunscape/arch-install-config/master/configs/home/.config/kitty/kitty.conf
-	
-	# libinput gestures
-	# curl -Lo $HOMEDIR/.config/libinput-gestures.conf https://raw.githubusercontent.com/Arunscape/arch-install-config/master/configs/home/.config/libinput-gestures.conf
+# make directories as necessary
+# mkdir -p $HOMEDIR/.config/kitty
+# mkdir -p $HOMEDIR/.config/sway
 
-	# .Xresources
-	# curl -Lo $HOMEDIR/.Xresources https://raw.githubusercontent.com/Arunscape/arch-install-config/master/configs/home/.Xresources
+# sway config
+# curl -Lo $HOMEDIR/.config/sway/config https://raw.githubusercontent.com/Arunscape/arch-install-config/master/configs/home/.config/sway/config
 
-	# .vimrc
-	# curl -Lo $HOMEDIR/.vimrc https://raw.githubusercontent.com/Arunscape/arch-install-config/master/configs/home/.vimrc
-	
-	# vim-plug
-	# just realized I have a section in .vimrc which sets this up if it's missing
-	#curl -Lo $HOMEDIR/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+# kitty
+# curl -Lo $HOMEDIR/.config/kitty/kitty.conf https://raw.githubusercontent.com/Arunscape/arch-install-config/master/configs/home/.config/kitty/kitty.conf
 
-	# doesn't work as nicely, should also be run as user postinstall I think
-	# echo Installing vim plugins...
-	# vim +PlugInstall +qall
+# libinput gestures
+# curl -Lo $HOMEDIR/.config/libinput-gestures.conf https://raw.githubusercontent.com/Arunscape/arch-install-config/master/configs/home/.config/libinput-gestures.conf
 
-	# .xinitrc
-	# curl -Lo $HOMEDIR/.xinitrc https://raw.githubusercontent.com/Arunscape/arch-install-config/master/configs/home/.xinitrc
-	
-	# wallpaper
-	# curl -Lo $HOMEDIR/Excision_APEX_3840x2160_Wallpaper.jpg https://github.com/Arunscape/arch-install-config/raw/master/configs/home/Excision_APEX_3840x2160_Wallpaper.jpg
+# .Xresources
+# curl -Lo $HOMEDIR/.Xresources https://raw.githubusercontent.com/Arunscape/arch-install-config/master/configs/home/.Xresources
+
+# .vimrc
+# curl -Lo $HOMEDIR/.vimrc https://raw.githubusercontent.com/Arunscape/arch-install-config/master/configs/home/.vimrc
+
+# vim-plug
+# just realized I have a section in .vimrc which sets this up if it's missing
+#curl -Lo $HOMEDIR/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+# doesn't work as nicely, should also be run as user postinstall I think
+# echo Installing vim plugins...
+# vim +PlugInstall +qall
+
+# .xinitrc
+# curl -Lo $HOMEDIR/.xinitrc https://raw.githubusercontent.com/Arunscape/arch-install-config/master/configs/home/.xinitrc
+
+# wallpaper
+# curl -Lo $HOMEDIR/Excision_APEX_3840x2160_Wallpaper.jpg https://github.com/Arunscape/arch-install-config/raw/master/configs/home/Excision_APEX_3840x2160_Wallpaper.jpg
 # }
 
 finish(){
-	# exit and reboot
-	rm /mnt/install.sh
-	umount -R /mnt
-	echo "$(tput bold)$(tput setaf 2)Done!!!$(tput sgr 0)"
-	# reboot
+    # exit and reboot
+    rm /mnt/install.sh
+    umount -R /mnt
+    echo "$(tput bold)$(tput setaf 2)Done!!!$(tput sgr 0)"
+    # reboot
 }
 
 setup
--- install-scripts/02-chroot.sh
+++ install-scripts/02-chroot.sh
@@ -9,49 +9,49 @@ CPU=$6
 WIFI=$7
 
 setup(){
-
-	# Timezone
-	ln -sf /usr/share/zoneinfo/$TIMEZONE /etc/localtime
-
-	# Run hwclock(8) to generate /etc/adjtime:
-	hwclock --systohc
-
-	# Hostname
-	echo $HOST_NAME > /etc/hostname
-
-	# Set root passwwd
-	#(
-	#echo $ROOT_PASSWD
-	#echo $ROOT_PASSWD
-	#) | passwd
-
-	# Localization
-	echo Uncommenting these lines in /etc/locale.gen:
-	cat /etc/locale.gen | grep en_CA
-	cat /etc/locale.gen | grep en_US
-	sed -i '/en_CA/ s/^#//' /etc/locale.gen
-	sed -i '/en_US/ s/^#//' /etc/locale.gen
-
-	echo LANG=en_CA.UTF-8 > /etc/locale.conf
-
-	echo Generating locale...
-	locale-gen
-
-	# making directories if necessary
-	mkdir -p boot/loader/entries/
-
-	# systemd
-	echo Setting up systemd-boot...
-
+    
+    # Timezone
+    ln -sf /usr/share/zoneinfo/$TIMEZONE /etc/localtime
+    
+    # Run hwclock(8) to generate /etc/adjtime:
+    hwclock --systohc
+    
+    # Hostname
+    echo $HOST_NAME > /etc/hostname
+    
+    # Set root passwwd
+    #(
+    #echo $ROOT_PASSWD
+    #echo $ROOT_PASSWD
+    #) | passwd
+    
+    # Localization
+    echo Uncommenting these lines in /etc/locale.gen:
+    cat /etc/locale.gen | grep en_CA
+    cat /etc/locale.gen | grep en_US
+    sed -i '/en_CA/ s/^#//' /etc/locale.gen
+    sed -i '/en_US/ s/^#//' /etc/locale.gen
+    
+    echo LANG=en_CA.UTF-8 > /etc/locale.conf
+    
+    echo Generating locale...
+    locale-gen
+    
+    # making directories if necessary
+    mkdir -p boot/loader/entries/
+    
+    # systemd
+    echo Setting up systemd-boot...
+    
 	cat > boot/loader/loader.conf << EOF
 default arch
 timeout 1
 editor no
 auto-entries 0
 EOF
-
-	local rootpart="$DRIVE"2
-	diskuuid=$(blkid -s PARTUUID -o value $rootpart)
+    
+    local rootpart="$DRIVE"2
+    diskuuid=$(blkid -s PARTUUID -o value $rootpart)
 	cat > boot/loader/entries/arch.conf << EOF
 title   Arch Linux
 linux   /vmlinuz-linux
@@ -59,7 +59,7 @@ initrd  /$CPU-ucode.img
 initrd  /initramfs-linux.img
 options root=PARTUUID=$diskuuid rw
 EOF
-
+    
 	cat > boot/loader/entries/arch-lts.conf << EOF
 title   Arch Linux LTS Kernel
 linux   /vmlinuz-linux-lts
@@ -67,11 +67,11 @@ initrd  /$CPU-ucode.img
 initrd  /initramfs-linux-lts.img
 options root=PARTUUID=$diskuuid rw
 EOF
-
-	bootctl install
-
-# hook to run bootctl update whenever systemd is updated
-	mkdir -p /etc/pacman.d/hooks
+    
+    bootctl install
+    
+    # hook to run bootctl update whenever systemd is updated
+    mkdir -p /etc/pacman.d/hooks
 	cat > /etc/pacman.d/hooks/systemd-boot.hook << EOF
 [Trigger]
 Type = Package
@@ -83,98 +83,98 @@ Description = Updating systemd-boot
 When = PostTransaction
 Exec = /usr/bin/bootctl update
 EOF
-
-	# add user
-	echo Creating user $USERNAME
-	useradd -m -g wheel $USERNAME
-	(
-	echo $USER_PASSWD
-	echo $USER_PASSWD
-	) | passwd $USERNAME
-
-	# edit sudoers file to allow wheel users to run sudo commands
-	sed -i '/%wheel ALL=(ALL) ALL/ s/^# //' /etc/sudoers
-	echo Here\'s what got uncommented:
-	cat /etc/sudoers | grep wheel
-
-	# Disable root account
-	usermod -p '!' root
-
-	# makes pacman and yay colourful
-	sed -i "s/^#Color/Color/" /etc/pacman.conf
-	sed -i "/^Color/a ILoveCandy" /etc/pacman.conf
-
+    
+    # add user
+    echo Creating user $USERNAME
+    useradd -m -g wheel $USERNAME
+    (
+        echo $USER_PASSWD
+        echo $USER_PASSWD
+    ) | passwd $USERNAME
+    
+    # edit sudoers file to allow wheel users to run sudo commands
+    sed -i '/%wheel ALL=(ALL) ALL/ s/^# //' /etc/sudoers
+    echo Here\'s what got uncommented:
+    cat /etc/sudoers | grep wheel
+    
+    # Disable root account
+    usermod -p '!' root
+    
+    # makes pacman and yay colourful
+    sed -i "s/^#Color/Color/" /etc/pacman.conf
+    sed -i "/^Color/a ILoveCandy" /etc/pacman.conf
+    
 }
 
 install_stuff(){
-
-	# rank mirrors
-	#pacman -S --noconfirm pacman-contrib
-	#echo 'Ranking mirrors.. This will take a while...'
-	#cp /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist.bakup
-	#curl https://www.archlinux.org/mirrorlist/all/https/ | sed -e 's/^#Server/Server/' -e '/^#/d' | rankmirrors -n 6 - > /etc/pacman.d/mirrorlist
-
-	# install things
-	echo Installing stuff...
-		pacman -S --noconfirm --needed\
-		vim \
-		git \
-		$CPU-ucode \
-		linux-headers \
-		linux-lts \
-		ntfs-3g \
-		kitty \
-		pulseaudio \
-		gnome-keyring \
-		libsecret \
-		wl-clipboard \
-		slurp \
-		grim \
-		ttf-joypixels
-		# firefox-developer-edition
-
-		echo Installing sway and wlroots because somehow the order matters
-		pacman -S --noconfirm --needed wlroots
-		pacman -S --noconfirm --needed sway
-
-
-		echo Installing yay...
-		git clone https://aur.archlinux.org/yay.git
-		chmod 777 -R yay
-		cd yay
-		sudo -u $USERNAME makepkg --noconfirm -si
-		cd ..
-		rm -rf yay
-
-		echo Installing stuff from AUR...
-		sudo -u $USERNAME yay -S --noconfirm --needed \
-		nerd-fonts-fira-code
-		# flat-remix-git \
-		# universal-ctags-git \
-
-		if [ -z "$WIFI"] then
-			:
-		else
-			pacman -S --noconfirm --needed\
-			connman \
-			wpa_supplicant
-
-			yay -S --noconfirm --needed
-			libinput-gestures \
-			brillo
-			# doesn't work, see https://superuser.com/questions/688733/start-a-systemd-service-inside-chroot
-			# systemctl enable connman
-
-			gpasswd -a $USERNAME input
-			sudo -u $USERNAME libinput-gestures-setup autostart
-			sudo -u $USERNAME libinput-gestures-setup start
-		fi
-		
-		sudo -u $USERNAME git config --global credential.helper /usr/lib/git-core/git-credential-libsecret
+    
+    # rank mirrors
+    #pacman -S --noconfirm pacman-contrib
+    #echo 'Ranking mirrors.. This will take a while...'
+    #cp /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist.bakup
+    #curl https://www.archlinux.org/mirrorlist/all/https/ | sed -e 's/^#Server/Server/' -e '/^#/d' | rankmirrors -n 6 - > /etc/pacman.d/mirrorlist
+    
+    # install things
+    echo Installing stuff...
+    pacman -S --noconfirm --needed\
+    vim \
+    git \
+    $CPU-ucode \
+    linux-headers \
+    linux-lts \
+    ntfs-3g \
+    kitty \
+    pulseaudio \
+    gnome-keyring \
+    libsecret \
+    wl-clipboard \
+    slurp \
+    grim \
+    ttf-joypixels
+    # firefox-developer-edition
+    
+    echo Installing sway and wlroots because somehow the order matters
+    pacman -S --noconfirm --needed wlroots
+    pacman -S --noconfirm --needed sway
+    
+    
+    echo Installing yay...
+    git clone https://aur.archlinux.org/yay.git
+    chmod 777 -R yay
+    cd yay
+    sudo -u $USERNAME makepkg --noconfirm -si
+    cd ..
+    rm -rf yay
+    
+    echo Installing stuff from AUR...
+    sudo -u $USERNAME yay -S --noconfirm --needed \
+    nerd-fonts-fira-code
+    # flat-remix-git \
+    # universal-ctags-git \
+    
+    if [ -z "$WIFI"] then
+        :
+    else
+        pacman -S --noconfirm --needed\
+        connman \
+        wpa_supplicant
+        
+        yay -S --noconfirm --needed \
+        libinput-gestures \
+        brillo
+        # doesn't work, see https://superuser.com/questions/688733/start-a-systemd-service-inside-chroot
+        # systemctl enable connman
+        
+        gpasswd -a $USERNAME input
+        sudo -u $USERNAME libinput-gestures-setup autostart
+        sudo -u $USERNAME libinput-gestures-setup start
+    fi
+    
+    sudo -u $USERNAME git config --global credential.helper /usr/lib/git-core/git-credential-libsecret
 }
 
 postinstall(){
-	echo TODO
+    echo TODO
 }
 
 

debe4c0df87bd2d88455ccac674792c186938b90
Arun Woosaree <arun@woosaree.xyz>
2020-04-30 23:39:36 -0600

    Update 01-install.sh

--- install-scripts/01-install.sh
+++ install-scripts/01-install.sh
@@ -99,7 +99,7 @@ format(){
 
     sgdisk --clear \
          --new=1:0:+512MiB --typecode=1:ef00 --change-name=1:EFI \
-         --new=2:0:0       --typecode=3:8300 --change-name=3:cryptsystem \
+         --new=2:0:0       --typecode=2:8300 --change-name=2:cryptsystem \
            $DRIVE
 
     mkfs.fat -F32 -n EFI /dev/disk/by-partlabel/EFI
